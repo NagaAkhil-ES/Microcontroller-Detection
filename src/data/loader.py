5a0d8ff7ac4b50f5470e2e2a3430909d369f7327
Naga Akhil ES <nagaakhil.es@ltts.com>
2022-08-18 16:45:01 +0530

    +, revised dataloader

--- src/data/loader.py
+++ src/data/loader.py
@@ -4,9 +4,10 @@ import pandas as pd
 from torch.utils.data import DataLoader
 from PIL import ImageDraw
 from torchvision.transforms import ToPILImage
+from torch import stack as t_stack
 
 from data.dataset import CustomDataset_df
-from data.transforms import get_train_transforms, get_test_transforms
+from data.transforms import get_transforms
 from util.utils import setup_save_dir
 
 def get_data_loader(params, mode, normalize=True):
@@ -23,44 +24,48 @@ def get_data_loader(params, mode, normalize=True):
     """
     if mode == "train":
         csv_path = params.train_csv_path
-        tv_transforms = get_train_transforms(params, normalize)
+        transforms = get_transforms(params, is_train=True, normalize=normalize)
     elif mode == "val":
         csv_path = params.val_csv_path
-        tv_transforms = get_test_transforms(params, normalize)
+        transforms = get_transforms(params, is_train=False, normalize=normalize)
     # data loader pipeline
     meta_df = pd.read_csv(csv_path)
-    d_set = CustomDataset_df(meta_df, params.classes, params.frame_shape, tv_transforms)
+    d_set = CustomDataset_df(meta_df, params.classes, transforms)
     d_loader = DataLoader(d_set, batch_size=params.batch_size, shuffle=False, 
-                            num_workers=params.num_workers) #, collate_fn=collate_fn)
+                            num_workers=params.num_workers, collate_fn=collate_fn)
     return d_loader          
 
 def collate_fn(batch):
-    return tuple(zip(*batch))
+    sample = list(zip(*batch))
+    sample[0] = t_stack(sample[0])
+    return sample
 
-def save_transformed_images(d_loader, save_dir):
+def save_transformed_images(images, targets, save_dir):
     # import pdb; pdb.set_trace()
-
-    images, targets = next(iter(d_loader))
-    print("dataloader item:")
-    print(images.shape)
-    print(targets)
-    print(f"Images Min: {images.min()}, Max: {images.max()}")
-    
-    # save transformed image
-    boxes = targets["boxes"].int().tolist()
-    image_ids = targets["image_id"].squeeze().tolist()
-    setup_save_dir(save_dir)
-    for i, image in enumerate(images):
-        image = ToPILImage()(image)
-        img1 = ImageDraw.Draw(image)
-        for box in boxes[i]:
-            img1.rectangle(box, outline="red")
-        save_path = os.path.join(save_dir, f"{image_ids[i]}.png")
-        image.save(save_path)
+    for i, target in enumerate(targets):
+        image = ToPILImage()(images[i])
+        img_draw = ImageDraw.Draw(image)
+        for box in target["boxes"]:
+            img_draw.rectangle(box.tolist(), outline="red")
+        
+        setup_save_dir(save_dir)
+        image_id = int(target["image_id"])
+        save_path = os.path.join(save_dir, f"{image_id}.png")
+        image.save(save_path)    
     
 # Main block
 if __name__ == "__main__":
     from util.config import get_configs
-    params = get_configs("configs/run.yaml", "configs/params.yaml", f_show=True)
+
+    params = get_configs("configs/run.yaml", "configs/params.yaml", f_show=False)
     dl = get_data_loader(params, mode="train", normalize=False)
-    save_transformed_images(dl, "reports/data_loader/test6")
+
+    # Show properties of an item
+    images, targets = next(iter(dl))
+    print("dataloader item:","\n")
+    print("img")
+    print(type(images), images.dtype, images.shape, images.min(), images.max(), "\n")
+    print("target", type(targets))
+    print(targets)
+
+    save_transformed_images(images, targets, "reports/data_loader/album/test1")
