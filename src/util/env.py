1aa8404162e6ac9bdc5ba54fd5d234d3d623ab83
Naga Akhil ES <nagaakhil.es@ltts.com>
2022-08-26 12:29:29 +0530

    r, added use deter algo flag

--- src/util/env.py
+++ src/util/env.py
@@ -3,18 +3,23 @@ import os
 import random
 import numpy as np
 
-def setup_deterministic_env(seed=137):
+def seed_everything(seed=137, use_deterministic_algorithms=False):
     """ To set random seed for deterministic training to reproduce same results
 
     Args:
         seed (int, optional): seed value. Defaults to 3.
     """
-    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    # seed everything
     os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ["PL_GLOBAL_SEED"] = str(seed)
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+
+    # set deterministic algo
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
-    torch.use_deterministic_algorithms(True)
+    if use_deterministic_algorithms:
+        os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+        torch.use_deterministic_algorithms(True)
