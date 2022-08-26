6c57c32886183b74a39e39a5a218a1e8f8f85af7
Naga Akhil ES <nagaakhil.es@ltts.com>
2022-08-26 12:37:47 +0530

    +, updated seed everything in train.py

--- src/train/train.py
+++ src/train/train.py
@@ -1,13 +1,13 @@
 from util.config import get_configs
 from data.loader import get_data_loader
 from model.loader import get_model
-from util.env import setup_deterministic_env
+from util.env import seed_everything
 import pytorch_lightning as pl
 
 if __name__ == '__main__':
     # load parameters and run args
     params = get_configs("configs/run.yaml", "configs/params.yaml", f_show=True)
-    setup_deterministic_env(params.random_seed)
+    seed_everything(params.random_seed)
     
     # Dataloader
     train_loader = get_data_loader(params, mode="train")
@@ -19,5 +19,5 @@ if __name__ == '__main__':
     # Trainer
     trainer = pl.Trainer(accelerator=params.accelerator, devices=params.devices, 
                         max_epochs=params.max_epochs)
-    trainer.fit(model=model, train_dataloaders=train_loader, 
+    trainer.fit(model=model, train_dataloaders=train_loader,
                 val_dataloaders=val_loader)
